a33fe32f82a082cecc774b2b8d1772aa89272efe
Campbell longmire <campbell.longmire@gmail.com>
2019-08-05 09:22:47 +1200

    Update Game.java

--- Game.java
+++ Game.java
@@ -9,153 +9,204 @@ import java.util.*;
 
 public class Game {
 
-    enum CharacterEnum implements Card{
-        MrsWhite,MrGreen,MrsPeacock,ProfessorPlum,ColonelMustard,MissScarlett;
-    }
-    enum WeaponEnum implements Card{
-        Candlestick,Dagger,LeadPipe,Revolver,Rope,Spanner;
-    }
-    enum RoomEnum implements Card{
-        KITCHEN,HALL,STUDY,LOUNGE,CONSERVATORY,BILLIARDROOM,LIBRARY,DININGROOM;
+    enum CharacterEnum implements Card {
+        MrsWhite, MrGreen, MrsPeacock, ProfessorPlum, ColonelMustard, MissScarlett;
     }
 
+    enum WeaponEnum implements Card {
+        Candlestick, Dagger, LeadPipe, Revolver, Rope, Spanner;
+    }
 
-
+    enum RoomEnum implements Card {
+        KITCHEN, HALL, STUDY, LOUNGE, CONSERVATORY, BILLIARDROOM, LIBRARY, DININGROOM;
+    }
 
 
     ArrayList<Card> deck = new ArrayList<Card>();
     ArrayList<Player> players = new ArrayList<Player>();
     boolean gamewon = false;
     ArrayList<Card> Solution = new ArrayList<Card>();
+    Board board = new Board();
 
 
-    public void playClue(){
+    public void playClue() {
 
-        Board board = new Board();
+
         board.makeBoard();
 
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Welcome to Cluedo, please enter the number of players you would like to play with (between 3 and 6): ");
-        int n = reader.nextInt();
-        while (n<3 || n>6){
-            System.out.println("You did not enter a number between 3 and 6, please try again:");
-            n = reader.nextInt();
-        }
-
-        while (n-1>=0){
-            Location loc = board.getPlayerPos(n-1);           //Put players on the board
-            String name = CharacterEnum.values()[n-1].toString();
-            Position pos  = board.getPos(loc);
-            board.setPosName(loc,name);
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Welcome to Cluedo, please enter the number of players you would like to play with (between 3 and 6): ");
+//        int n = reader.nextInt();
+//        while (n<3 || n>6){
+//            System.out.println("You did not enter a number between 3 and 6, please try again:");
+//            n = reader.nextInt();
+//        }
+        int m = 4;
+        while (m - 1 >= 0) {
+            Location loc = board.getPlayerPos(m - 1);           //Put players on the board
+            String name = CharacterEnum.values()[m - 1].toString();
+            Position pos = board.getPos(loc);
+            board.setPosName(loc, name);
             board.setOccupied(loc);
 
 
-            Player play = new Player(loc, name, new ArrayList<Card>(), false ); // Creates new player
+            Player play = new Player(loc, name, new ArrayList<Card>(), false); // Creates new player
             players.add(play);
 
 
-            n--;
+            //  n--;
+            m--;
         }
 
 
         board.printBoard();
-        reader.close();
+        //reader.close();
 
         MakeMurderer();
         Makedeck();
         DrawCards();
         PlayersTurns(players.get(1));
 
-//        while(gamewon == false){
-//            for(int a = 0; a < players.size(); a++){
+
+
+
+
+
+//        while(gamewon == false) {
+//            for (int a = 0; a < players.size(); a++) {
 //                PlayersTurns(players.get(a));
 //
 //            }
+//        }
 
-        }
-
-
-
+    }
 
 
+    public void PlayersTurns(Player player) {
 
 
-        public void PlayersTurns(Player player){
-            int RoleNums = (int) (Math.random()*12+1)-1;
-            if(RoleNums <3)RoleNums = 3;
 
 
-            System.out.println("It is your turn to move "+player.getName()+ ". You have "+ RoleNums + " moves left.");
 
-            JFrame frame = new JFrame("Key Listener");
+        JFrame frame = new JFrame("Key Listener");
 
-            Container contentPane = frame.getContentPane();
+        Container contentPane = frame.getContentPane();
 
-            KeyListener listener = new KeyListener() {
+        KeyListener listener = new KeyListener() {
 
-                @Override
 
-                public void keyPressed(KeyEvent event) {
+            @Override
+            public void keyPressed(KeyEvent event) {
 
-                    int key = event.getKeyCode();
-                    if (key == KeyEvent.VK_LEFT) {
-                        Location loca = player.getloc();
-                        loca.setX(loca.getX()-1);
-                        player.setLoc(loca);
-                    }
+
+
+
+                int key = event.getKeyCode();
+
+                Location loc = player.getloc();
+                Location newloc = null;
+
+                if (key == 37) {
+                    int x = loc.getX() - 1;
+                    newloc = new Location(x, loc.getY());
+                } else if (key == 39) {
+                    int x = loc.getX() + 1;
+                    newloc = new Location(x, loc.getY());
                 }
-
-                @Override
-                public void keyReleased(KeyEvent event) {
+                else if (key == 40) {
+                    int x = loc.getY() + 1;
+                    newloc = new Location(loc.getX(), x);
                 }
-
-                @Override
-                public void keyTyped(KeyEvent event) {
-                }
-            };
-
-            JTextField textField = new JTextField();
-            textField.addKeyListener(listener);
-            contentPane.add(textField, BorderLayout.NORTH);
-            frame.pack();
-            frame.setVisible(true);
-
-
-
-        }
-
-
-
-
-
-
-        public void DrawCards() {
-
-        for(int b = 0; b<Solution.size();b++){
-            deck.remove(Solution.get(b));
-        }
-
-            int player = 0;
-            while (deck.size() >0){
-                int random = (int) (Math.random()*deck.size()+1)-1;
-                players.get(player).addCard(deck.get(random));
-                deck.remove(random);
-                if(player == players.size()-1){
-                    player = 0;
-                }
-                else{
-                    player++;
+                else if (key == 38) {
+                    int x = loc.getY() - 1;
+                    newloc = new Location(loc.getX(), x);
                 }
 
 
 
+
+                if (canMove(player, newloc)) {
+
+
+                    board.setPosName(loc, "");
+                    board.setNotoccupied(loc);
+                    board.setPosName(newloc, player.getName());
+                    board.setOccupied(newloc);
+                    player.setLoc(newloc);
+
+                    board.printBoard();
+
+                }
 
 
             }
 
+
+            @Override
+            public void keyReleased(KeyEvent event) {
+
+            }
+
+            @Override
+            public void keyTyped(KeyEvent event) {
+
+            }
+        };
+
+
+        JTextField textField = new JTextField();
+        textField.addKeyListener(listener);
+        contentPane.add(textField, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+
+    public void makeSuggestion(){
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What Weapon do you think the Murder used? Select the number corresponding to weapon on screen");
+        for(int a = 0; a<WeaponEnum.values().length;a++){
+            System.out.println(a+": "+WeaponEnum.values()[a].toString());
+        }
+        int n = reader.nextInt();
+
+    }
+
+    public boolean canMove(Player player, Location loc) {
+
+        Location loc1 = player.getloc();
+
+        if (board.getPos(loc).getType().equals(board.getPos(loc1).getType())) {
+            return true;
         }
 
+        return false;
+    }
 
+
+    public void DrawCards() {
+
+        for (int b = 0; b < Solution.size(); b++) {
+            deck.remove(Solution.get(b));
+        }
+
+        int player = 0;
+        while (deck.size() > 0) {
+            int random = (int) (Math.random() * deck.size() + 1) - 1;
+            players.get(player).addCard(deck.get(random));
+            deck.remove(random);
+            if (player == players.size() - 1) {
+                player = 0;
+            } else {
+                player++;
+            }
+
+
+        }
+
+    }
 
 
     public void MakeMurderer() {
@@ -169,7 +220,7 @@ public class Game {
         Solution.add(MurderWeapon);
 
         int randomRoom = (int) (Math.random() * RoomEnum.values().length + 1) - 1;
-        Card MurderRoom= RoomEnum.values()[randomWeapon];
+        Card MurderRoom = RoomEnum.values()[randomWeapon];
         Solution.add(MurderRoom);
 
 
@@ -186,14 +237,7 @@ public class Game {
     }
 
 
-
-
-
-
-
-
-
-    public static void main (String Args[]){
+    public static void main(String Args[]) {
         Game a = new Game();
         a.playClue();
     }

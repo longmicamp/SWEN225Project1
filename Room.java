188098a5b4ac4379a5ae6d8bb3474305c4d2c311
Campbell longmire <campbell.longmire@gmail.com>
2019-08-05 14:22:56 +1200

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,2 +1,32 @@
 public class Room {
+
+    private String Weapon;
+    private String Room;
+
+
+
+    public Room(String room) {
+        super();
+        Room = room;
+    }
+
+    public String getRoom() {
+        return Room;
+    }
+
+    public void setRoom(String room) {
+        Room = room;
+    }
+
+    public String getWeapon() {
+        return Weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        Weapon = weapon;
+    }
 }
+
+
+
+
